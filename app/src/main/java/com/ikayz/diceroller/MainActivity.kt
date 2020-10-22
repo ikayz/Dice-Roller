990247f6b99e5eb31c247e5926ba822f512b6278
Isaac Miti <ikayz@live.com>
2020-10-22 23:24:03 +0200

    Add lateinit and refactor calling findviewby id

--- app/src/main/java/com/ikayz/diceroller/MainActivity.kt
+++ app/src/main/java/com/ikayz/diceroller/MainActivity.kt
@@ -9,18 +9,20 @@ import android.widget.Toast
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+    lateinit var diceImage: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.text = "Let's roll"
+        diceImage = findViewById(R.id.dice_image)
         rollButton.setOnClickListener() {
             rollDice()
         }
     }
 
     private fun rollDice() {
-        val diceImage: ImageView = findViewById(R.id.dice_image)
         val drawableResource = when(Random.nextInt(6)+ 1) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
